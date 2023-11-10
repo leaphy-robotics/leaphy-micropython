f0590f8036a384dc7b0cecd943ff25379b7edf2f
Julius0530 <julius.vanwegen@gmail.com>
2023-11-10 15:08:55 +0100

    chore: move up the the src/ dir and __init__.py to the root dir

--- leaphy_micropython/__init__.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-# IMPORTS
-from .src.PWM import setPWM, readPWM, set_pin
-from .src.Servo import servo_angle
-from .src.Sonar import read_distance
-from .src.Buzzer import set_buzzer
-# END IMPORTS
-
