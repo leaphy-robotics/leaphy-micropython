bd02c890ec9d9637cd81809dcbd4dddfdfea2bd0
Julius0530 <julius.vanwegen@gmail.com>
2023-11-10 15:10:41 +0100

    enhancement: actually bump up and unsaved files

---
+++ src/Buzzer.py
@@ -0,0 +1,9 @@
+from machine import Pin,PWM
+from .boards_config import pinToGPIO
+
+def set_buzzer(pin : int, loud : int):
+    """Turns a buzzer and you can choose how hard the sound is."""
+    buzzerpin = pinToGPIO(pin)
+    buzzer = PWM(Pin(buzzerpin))
+    if loud < 0 or loud > 255: raise ValueError("Buzzer values must be in between 0 and 255")
+    buzzer.duty_u16(loud * 257)
