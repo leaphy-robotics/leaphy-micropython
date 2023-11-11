f221eb1bead8f7d05a7d27ad0b444f976eb6bfdf
koen1711 <za4266468@gmail.com>
2023-11-11 08:54:49 +0100

    fix: organized python module; add package.json content

--- actuators/buzzer/Buzzer.py
+++ actuators/buzzer/Buzzer.py
@@ -1,10 +1,10 @@
 from machine import Pin, PWM
-from .boards_config import pinToGPIO
+from boards_config import pinToGPIO
 
 def set_buzzer(pin : int, loud : int):
     """Turns a buzzer and you can choose how hard the sound is."""
     buzzerpin = pinToGPIO(pin)
     buzzer = PWM(Pin(buzzerpin))
     buzzer.freq(255)
-    if loud < 0 or loud > 255: raise ValueError("Buzzer values must be in between 0 and 255")
+    if loud < 0 or loud > 255: raise ValueError("buzzer values must be in between 0 and 255")
     buzzer.duty_u16(loud * 257)
