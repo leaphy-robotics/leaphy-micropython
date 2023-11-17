bff68722ce494678a2fc790489bbfdbdd818efe4
Julius0530 <julius.vanwegen@gmail.com>
2023-11-17 15:20:34 +0100

    feat: add line sensor

---
+++ sensors/linesensor.py
@@ -0,0 +1,15 @@
+"""A module for the new leaphy line sensor"""
+from machine import Pin
+from utils.boards_config import pin_to_gpio
+
+
+def read_line_sensor(pin: int) -> int:
+    """
+    Sets a pin
+    :param pin: int, the pin to set
+    :return: sensor_state: gives a value from the module
+    """
+    pin = pin_to_gpio(pin)
+    pin = Pin(pin, Pin.IN)
+    sensor_state = pin.value()
+    return sensor_state
