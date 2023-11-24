d0093087447d65f5775857d0fb493b7a96874e54
Julius0530 <julius.vanwegen@gmail.com>
2023-11-24 20:57:06 +0100

    fix: docstrings

--- utils/timeschedule.py
+++ utils/timeschedule.py
@@ -1,6 +1,7 @@
-"""import modules"""
 from time import sleep
 
 def sleep_minute(minutes: int):
-    """sleeps for a number of minutes"""
+    """
+    :param minutes: the amount of minutes
+    """
     sleep(60 * minutes)
