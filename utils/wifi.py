f42fc8a1552fcf7fd41ea012a05a8c6e199ecc24
Julius0530 <julius.vanwegen@gmail.com>
2023-11-27 13:19:49 +0100

    fix: issues

--- utils/wifi.py
+++ utils/wifi.py
@@ -1,12 +1,12 @@
 from time import sleep
 import network
 
-def connect(ssid: str, password: str) -> tuple[str]:
+def connect(ssid: str, password: str) -> str:
     """
     Connects the pico to the wifi
     :param ssid: gives the wifi name to the pico
     :param password: gives the password from the wifi to the pico
-    :return: ip_addresses: gives the ip addresses from the pico w
+    :return: ip_addresses: returns the address from the pico w
     """
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -14,4 +14,4 @@ def connect(ssid: str, password: str) -> tuple[str]:
     while not wlan.isconnected():
         sleep(1)
         print("Connecting to WiFi...")
-    return wlan.ifconfig()
+    return wlan.ifconfig()[0]
