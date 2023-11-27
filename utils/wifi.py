daf9122f3a08fcebae8b8a2c54ffc11cc79b5a04
Julius0530 <julius.vanwegen@gmail.com>
2023-11-27 12:16:57 +0100

    feat: docstrings added and the ip addresses

--- utils/wifi.py
+++ utils/wifi.py
@@ -1,10 +1,17 @@
-import network
 from time import sleep
+import network
 
 def connect(ssid, password):
+    """
+    Connects the pico to the wifi
+    :param ssid: str, gives the wifi name to the pico
+    :param password: str, gives the password from the wifi to the pico
+    :return: ip_addresses: tuple[str]int, gives the ip addresses from the pico w
+    """
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
     while not wlan.isconnected():
         sleep(1)
         print("Connecting to WiFi...")
+    return wlan.ifconfig()
