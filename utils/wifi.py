607ac0b66901892828976a98ee6d146f252674a8
Julius0530 <julius.vanwegen@gmail.com>
2023-11-27 12:22:07 +0100

    fix: docstrings

--- utils/wifi.py
+++ utils/wifi.py
@@ -1,12 +1,12 @@
 from time import sleep
 import network
 
-def connect(ssid, password):
+def connect(ssid: str, password: str) -> tuple[str]:
     """
     Connects the pico to the wifi
-    :param ssid: str, gives the wifi name to the pico
-    :param password: str, gives the password from the wifi to the pico
-    :return: ip_addresses: tuple[str]int, gives the ip addresses from the pico w
+    :param ssid: gives the wifi name to the pico
+    :param password: gives the password from the wifi to the pico
+    :return: ip_addresses: gives the ip addresses from the pico w
     """
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
