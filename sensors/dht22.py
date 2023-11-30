571f5fbe0e981971faffa5632529a85cb0683b5f
Julius0530 <102292563+Julius0530@users.noreply.github.com>
2023-11-30 12:54:27 +0100

    feat: working code for the dht22 (#14)
    * feat: working code for the dht22
    
    * fix: json package
    
    * Update dht22.py
    
    * Update package.json
    
    * fix: last final changes
    
    ---------
    
    Co-authored-by: Koen <98043234+koen1711@users.noreply.github.com>

---
+++ sensors/dht22.py
@@ -0,0 +1,30 @@
+import dht
+import machine
+from leaphymicropython.utils.boards.config import pin_to_gpio
+
+class DHT22():
+    """
+    A class to control the dht22
+    """
+    def __init__(self, pin: int):
+        """
+        Sets the pins for the dht22
+        :param pin: sets the pin for the dht22
+        """
+        pin = pin_to_gpio(pin)
+        self.sensor = dht.DHT22(machine.Pin(pin))
+    def read_temperature(self) -> float:
+        """
+        :return: temperature: gives the temperature in Celsius
+        """
+        self.sensor.measure()
+        temperature = self.sensor.temperature()
+        return temperature
+
+    def read_humidity(self) -> float:
+        """
+        :return: humidity: gives the humidity in percentages
+        """
+        self.sensor.measure()
+        humidity = self.read_humidity()
+        return humidity
--- utils/wifi.py
+++ utils/wifi.py
@@ -3,9 +3,9 @@ import network
 
 def connect(ssid: str, password: str) -> str:
     """
-    Connects the pico to the wifi
-    :param ssid: gives the wifi name to the pico
-    :param password: gives the password from the wifi to the pico
+    Connects the pico to the Wi-Fi
+    :param ssid: gives the Wi-Fi name to the pico
+    :param password: gives the password from the Wi-Fi to the pico
     :return: ip_addresses: returns the address from the pico w
     """
     wlan = network.WLAN(network.STA_IF)
