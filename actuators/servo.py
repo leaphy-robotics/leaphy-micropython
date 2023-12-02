7ef7582dec663087ee84b55f284e12fd8157921f
Julius0530 <102292563+Julius0530@users.noreply.github.com>
2023-12-02 11:01:20 +0100

    fix: better docstrings (#19)

--- actuators/servo.py
+++ actuators/servo.py
@@ -1,19 +1,18 @@
-from time import sleep
-
 from leaphymicropython.utils.boards_config import pin_to_gpio
 from machine import Pin, PWM
 
 
-def servo_angle(pin: int, angle: int):
-    """Puts the servo on a angle"""
+def servo_angle(pin: int, angle: int) -> None:
+    """
+    Puts the servo on an angle
+    :param pin: The pin number to which the servo motor is connected
+    :param angle: The angle to set the servo motor to
+    """
     pin = pin_to_gpio(pin)
     pwm = PWM(Pin(pin))
     pwm.freq(50)
     degrees_in_u16 = (9000 - 2000) / 180
-    u_16 = round(9000 - (angle * degrees_in_u16))
-    if not angle > -1 and not angle < 181:
+    duty_cycle = round(9000 - (angle * degrees_in_u16))
+    if not angle > -1 or not angle < 361:
         raise ValueError("The angle must be in between 0 and 180")
-    current_pos = pwm.duty_u16()
-    for pos in range(current_pos, u_16, 50 if current_pos < u_16 else -50):
-        pwm.duty_u16(pos)
-        sleep(0.01)
+    pwm.duty_u16(duty_cycle)
