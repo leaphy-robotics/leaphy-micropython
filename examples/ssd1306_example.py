299d8cee7a3b6680baf2c77d2c287c07abb3d8b0
Julius0530 <julius.vanwegen@gmail.com>
2024-05-04 12:14:03 +0200

    fix: ronalds commentaar

--- examples/ssd1306_example.py
+++ examples/ssd1306_example.py
@@ -1,11 +1,10 @@
-# pylint: skip-file
-import ssd1306
+from leaphymicropython.actuators.ssd1306 import SSD1306I2C
 from machine import Pin, SoftI2C
 
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
-oled_width = 128
-oled_height = 64
+oled_WIDTH = 128
+oled_HEIGHT = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
 oled.text("Hello, World 1!", 0, 0)
