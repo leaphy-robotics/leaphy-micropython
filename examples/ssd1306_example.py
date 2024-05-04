ae286a360ec2009d279c3e95623f7c3a16ec8ba4
Julius0530 <julius.vanwegen@gmail.com>
2024-05-04 11:29:28 +0200

    fix: black

--- examples/ssd1306_example.py
+++ examples/ssd1306_example.py
@@ -7,8 +7,8 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-oled.text('Hello, World 1!', 0, 0)
-oled.text('Hello, World 2!', 0, 10)
-oled.text('Hello, World 3!', 0, 20)
+oled.text("Hello, World 1!", 0, 0)
+oled.text("Hello, World 2!", 0, 10)
+oled.text("Hello, World 3!", 0, 20)
 
 oled.show()
