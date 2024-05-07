11d9685409c169beae0784c617290924d79e661b
Julius0530 <julius.vanwegen@gmail.com>
2024-05-07 14:04:16 +0200

    fix: code to let it work

--- examples/ssd1306_example.py
+++ examples/ssd1306_example.py
@@ -1,11 +1,12 @@
-from leaphymicropython.actuators.ssd1306 import SSD1306I2C
 from machine import Pin, SoftI2C
+
+from leaphymicropython.actuators import ssd1306
 
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
-oled_WIDTH = 128
-oled_HEIGHT = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+oled = ssd1306.SSD1306SPI(OLED_WIDTH, OLED_HEIGHT, i2c)
 
 oled.text("Hello, World 1!", 0, 0)
 oled.text("Hello, World 2!", 0, 10)
