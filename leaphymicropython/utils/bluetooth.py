0b9a7746f7050f6a858110f24150d875bd31af1c
Julius0530 <julius.vanwegen@gmail.com>
2024-05-17 21:06:15 +0200

    feat: add blauwetand

---
+++ leaphymicropython/utils/bluetooth.py
@@ -0,0 +1,21 @@
+from machine import UART
+
+
+class BLUETOOTH:
+    # pylint: disable=too-few-public-methods
+    """
+    A class for the use of bluetooth
+    """
+    def __init__(self, baudrate: int) -> None:
+        """
+        set baudrate
+        """
+        self.uart = UART(0, baudrate)
+
+    def read_uart(self) -> bytes:
+        """
+        Read uart from your bluetooth module
+        """
+        if self.uart.any() > 0:
+            data = self.uart.read()
+        return data
