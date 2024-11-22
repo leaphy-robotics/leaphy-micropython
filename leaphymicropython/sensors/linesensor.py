15a089e2c2e63ab332f7935c8d4f6cff78ea327f
Marten Postma <postmam@corderius.nl>
2024-11-22 10:23:49 +0100

    line sensor analog code (#50)

--- leaphymicropython/sensors/linesensor.py
+++ leaphymicropython/sensors/linesensor.py
@@ -1,5 +1,6 @@
 """A module for the new leaphy line sensor"""
 from leaphymicropython.utils.pins import read_pin
+from leaphymicropython.utils.pins import read_analog
 
 
 def read_line_sensor(pin: int) -> bool:
@@ -9,3 +10,28 @@ def read_line_sensor(pin: int) -> bool:
     :return: sensor_state: gives a value from the module
     """
     return read_pin(pin)
+
+
+def calibrate_analog_line_sensor(
+    pin: int, below_white: int, above_black: int, test="yes"
+):
+    """
+    Maps analog line sensor output to white | black | unclear
+
+    :param pin: int, the pin to set
+    :param below_white: int, below this value, the color is white
+    :param above_black: int, above this value, the color is black
+    :param test: str, if set to 'yes', the value and the mapped value are shown in the shell
+
+    :return: color: white | black | unclear
+    """
+    value = read_analog(pin)
+    color = "unclear"
+    if value < below_white:
+        color = "white"
+    elif value > above_black:
+        color = "black"
+
+    if test == "yes":
+        print(value, color)
+    return color
