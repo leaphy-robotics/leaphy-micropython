e44ae715de73855b523c9b065e5e2918663fbd6c
boutreet <102292563+boutreet@users.noreply.github.com>
2025-01-31 16:09:13 +0100

    feat: add i2c helpers (#55)

--- leaphymicropython/sensors/compass.py
+++ leaphymicropython/sensors/compass.py
@@ -1,7 +1,8 @@
-import struct
 import time
 
 from micropython import const
+
+from leaphymicropython.utils.i2c_helper import CBits, RegisterStruct
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/jposada202020/MicroPython_QMC5883L.git"
@@ -37,97 +38,6 @@ MODE_CONTINUOUS = const(0b01)
 mode_values = (MODE_STANDBY, MODE_CONTINUOUS)
 
 RESET_VALUE = const(0b01)
-
-
-class CBits:
-    """
-    Changes bits from a byte register
-    """
-
-    def __init__(
-        self,
-        num_bits: int,
-        register_address: int,
-        start_bit: int,
-        register_width=1,
-        lsb_first=True,
-    ) -> None:
-        self.bit_mask = ((1 << num_bits) - 1) << start_bit
-        self.register = register_address
-        self.star_bit = start_bit
-        self.lenght = register_width
-        self.lsb_first = lsb_first
-
-    def __get__(
-        self,
-        obj,
-        objtype=None,
-    ) -> int:
-        mem_value = obj.i2c.readfrom_mem(obj.address, self.register, self.lenght)
-
-        reg = 0
-        order = range(len(mem_value) - 1, -1, -1)
-        if not self.lsb_first:
-            order = reversed(order)
-        for i in order:
-            reg = (reg << 8) | mem_value[i]
-
-        reg = (reg & self.bit_mask) >> self.star_bit
-
-        return reg
-
-    def __set__(self, obj, value: int) -> None:
-        memory_value = obj.i2c.readfrom_mem(obj.address, self.register, self.lenght)
-
-        reg = 0
-        order = range(len(memory_value) - 1, -1, -1)
-        if not self.lsb_first:
-            order = range(0, len(memory_value))
-        for i in order:
-            reg = (reg << 8) | memory_value[i]
-        reg &= ~self.bit_mask
-
-        value <<= self.star_bit
-        reg |= value
-        reg = reg.to_bytes(self.lenght, "big")
-
-        obj.i2c.writeto_mem(obj.address, self.register, reg)
-
-
-class RegisterStruct:
-    """
-    Register Struct
-    """
-
-    def __init__(self, register_address: int, form: str) -> None:
-        self.format = form
-        self.register = register_address
-        self.lenght = struct.calcsize(form)
-
-    def __get__(
-        self,
-        obj,
-        objtype=None,
-    ):
-        if self.lenght <= 2:
-            value = struct.unpack(
-                self.format,
-                memoryview(
-                    obj.i2c.readfrom_mem(obj.address, self.register, self.lenght)
-                ),
-            )[0]
-        else:
-            value = struct.unpack(
-                self.format,
-                memoryview(
-                    obj.i2c.readfrom_mem(obj.address, self.register, self.lenght)
-                ),
-            )
-        return value
-
-    def __set__(self, obj, value):
-        mem_value = value.to_bytes(self.lenght, "big")
-        obj.i2c.writeto_mem(obj.address, self.register, mem_value)
 
 
 class QMC5883L:
