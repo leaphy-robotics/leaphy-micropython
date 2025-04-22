a3eb32deb889a847a4cb9cce6ad4aeb5eb816fd4
Marten Postma <postmam@corderius.nl>
2025-04-22 22:22:24 +0200

    a class for AnalogIR

--- leaphymicropython/sensors/linesensor.py
+++ leaphymicropython/sensors/linesensor.py
@@ -1,7 +1,46 @@
 """A module for the new leaphy line sensor"""
 
-from leaphymicropython.utils.pins import read_pin
-from leaphymicropython.utils.pins import read_analog
+from leaphymicropython.utils.pins import read_pin, read_analog, init_analog_pin
+
+
+class AnalogIR:
+    """
+    A class to control the analog ir sensor
+    """
+
+    def __init__(self, pin_name, above_black=4000):
+        """
+        Initializes the AnalogIR object.
+
+        Args:
+            pin_name: The pin connect to the A0 pin of the IR sensor
+            everything you provide to machine.Pin, you can provide here.
+            above_black
+            above_black (int, optional): the analog value is represented
+            as a 16-bit unsigned integer ranging from 0 to 65535.
+            Practically speaking, a reading of 0 means purely white is picked up
+            a value of 65535 means that the sensor only picks up black.
+            With this parameter, you can determine the threshold between a black
+            and a white line. From experience, above 4000 usually indicates
+            a black line, but obviously you can change this.
+        """
+        self.pin = init_analog_pin(pin_name)
+        self.above_black = above_black
+
+    def get_analog_value(self):
+        """
+        :return: the analog value
+        """
+        return self.pin.read_u16()
+
+    def black_or_white(self):
+        """
+        :return: black or white
+        Black is returned if the analog value is >= self.above_black
+        """
+        if self.get_analog_value() >= self.above_black:
+            return "black"
+        return "white"
 
 
 def read_line_sensor(pin: int) -> bool:
--- leaphymicropython/utils/pins.py
+++ leaphymicropython/utils/pins.py
@@ -2,6 +2,25 @@ from machine import Pin, PWM, ADC
 from leaphymicropython.utils.boards_config import pin_to_gpio
 
 
+def init_analog_pin(pin_name):
+    """
+    Initializes an analog pin using ADC
+    :param pin_name: the pin to check
+    :return: True if the pin is analog, ValueError otherwise
+    """
+    try:
+        pin = ADC(pin_name)
+        return pin
+    except ValueError as ex:
+        # ValueError has an attribute 'value', contrary to the belief of pylint
+        if ex.value == "Pin doesn't have ADC capabilities":  # pylint: disable=no-member
+            print(f"It is not possible to use pin {pin_name} in an analog way")
+            print(
+                "Please use a pin that has a label that starts with A, for example A0, A1, etc."
+            )
+        raise ex
+
+
 def set_pwm(pin: int, value: int, freq: int = 50):
     """Sets a pwm pin
     :param pin: int, the pin to set
