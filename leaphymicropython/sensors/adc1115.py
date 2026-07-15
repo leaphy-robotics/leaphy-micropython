80303dd45d28a9fe4c8e9a8f92022fb45f7c91cb
Wouter van den Heuvel <wouter@leaphy.nl>
2026-07-15 15:27:03 +0200

    feat: TCA9354 has per-pin controls. chore: code formatting cleanup.

--- leaphymicropython/sensors/adc1115.py
+++ leaphymicropython/sensors/adc1115.py
@@ -4,13 +4,14 @@ from leaphymicropython.utils.i2c_helper import handle_i2c_errors, I2CRegisterDev
 
 _RESULT_ADDRESS = const(0x00)
 _CONFIG_ADDRESS = const(0x01)
-_CONFIG_BASE_VALUE = const(0xc180)
+_CONFIG_BASE_VALUE = const(0xC180)
 _CONFIG_READY_STATUS = const(0x8000)
 _CONFIG_CHANNEL_OFFSET = const(12)
 _LOWTRESH_ADDRESS = const(0x02)
 _LOWTRESH_DEFAULT = const(0x0000)
 _HITRESH_ADDRESS = const(0x03)
 _HITRESH_DEFAULT = const(0x8000)
+
 
 class Adc1115(I2CRegisterDevice):
     def __init__(
@@ -20,7 +21,7 @@ class Adc1115(I2CRegisterDevice):
         scl_gpio_pin=13,
         bus_id=72,
         freq=400_000,
-        show_warnings=True
+        show_warnings=True,
     ):
         __super__(
             register_width=2,
@@ -31,17 +32,17 @@ class Adc1115(I2CRegisterDevice):
             scl_gpio_pin=scl_gpio_pin,
             bus_id=bus_id,
             freq=freq,
-            show_warnings=show_warnings
+            show_warnings=show_warnings,
         )
 
-    def readADC_single_ended(self,channel:int) -> int:
+    def readADC_single_ended(self, channel: int) -> int:
         # startADCReading
         if channel < 0 or channel > 3:
-            raise ValueError("Channel needs to be between 0 and 3, was "+str(channel))
+            raise ValueError("Channel needs to be between 0 and 3, was " + str(channel))
         config = _CONFIG_BASE_VALUE | (channel << _CONFIG_CHANNEL_OFFSET)
-        self.register_write(_CONFIG_ADDRESS,config)
-        self.register_write(_HITRESH_ADDRESS,_HITRESH_DEFAULT)
-        self.register_write(_LOWTRESH_ADDRESS,_LOWTRESH_DEFAULT)
+        self.register_write(_CONFIG_ADDRESS, config)
+        self.register_write(_HITRESH_ADDRESS, _HITRESH_DEFAULT)
+        self.register_write(_LOWTRESH_ADDRESS, _LOWTRESH_DEFAULT)
         # conversionComplete
         while (self.register_read(_CONFIG_ADDRESS) & _CONFIG_READY_STATUS) == 0:
             pass
@@ -51,4 +52,3 @@ class Adc1115(I2CRegisterDevice):
         if (result & const(0x8000)) != 0:
             return result - const(0x10000)
         return result
-
--- leaphymicropython/utils/tca9354.py
+++ leaphymicropython/utils/tca9354.py
@@ -10,6 +10,9 @@ TCA9354_REG_INPUT = const(0x00)
 TCA9354_REG_OUTPUT = const(0x01)
 TCA9354_REG_CONFIG = const(0x03)
 
+TCA9354_CONFIG_IN = const(1)
+TCA9354_CONFIG_OUT = const(0)
+
 
 class Tca9354(I2CRegisterDevice):
     """
@@ -25,11 +28,11 @@ class Tca9354(I2CRegisterDevice):
         scl_gpio_pin=13,
         bus_id=32,
         freq=400_000,
-        show_warnings=True
+        show_warnings=True,
     ):
         """
         Initialize the TCA9354 multiplexer.
-        
+
         Args:
             io_config: 8-bit bitfield describing the input/output configuration. 0 configures a pin as input, 1 configures a pin as output.
         """
@@ -42,28 +45,57 @@ class Tca9354(I2CRegisterDevice):
             scl_gpio_pin=scl_gpio_pin,
             bus_id=bus_id,
             freq=freq,
-            show_warnings=show_warnings
+            show_warnings=show_warnings,
         )
 
-        self._last_value = 0
         self.io_config = io_config
-    
+
     def begin(self):
         """
         Configure the multiplexer to its expected settings.
-        Call this before requesting or sending data for the 
+        Call this before requesting or sending data for the
         first time.
         """
-        self.register_write(TCA9354_REG_CONFIG,self.io_config)
-    
+        self.register_write(TCA9354_REG_CONFIG, self.io_config)
+
+    def configure_pin(self, pin, direction):
+        """
+        Configure one pin of the multiplexer as input/output.
+
+        Args:
+            pin: The number of the pin to be configured. Values outside the range [0..7] will be quietly ignored.
+            direction: One of TCA9354_CONFIG_IN or TCA9354_CONFIG_OUT, indicating if the pin should be configured as input/output.
+        """
+        if pin < 0 or pin > 7:
+            return
+
+        if direction == TCA9354_CONFIG_IN:
+            self.register_update(TCA9354_REG_CONFIG, (1 << pin), 0)
+            self.io_config |= 1 << pin
+        if direction == TCA9354_CONFIG_OUT:
+            self.register_update(TCA9354_REG_CONFIG, 0, (1 << pin))
+            self.io_config &= const(0xFF) ^ (1 << pin)
+
     def read(self) -> int:
         """
         Read the current output value. Note that pins configured as output
         always read as 0.
         """
-        return self.register_read(TCA9354_REG_INPUT)
+        return self.register_read(TCA9354_REG_INPUT) & const(0xFF)
 
-    def write(self,value):
+    def read_pin(self, pin) -> bool:
+        """
+        Read the specified pin. Pins outside the range [0..7] will always read as `False`.
+
+        Args:
+            pin: The number of the pin to read.
+        """
+        if pin < 0 or pin > 7:
+            return False
+        current_output = self.read()
+        return current_output & (1 << pin) != 0
+
+    def write(self, value):
         """
         Write a value to the output register. Note that only pins configured as output
         will update.
@@ -71,5 +103,19 @@ class Tca9354(I2CRegisterDevice):
         Args:
             value: The 8-bit value to write.
         """
-        self.register_write(TCA9354_REG_OUTPUT,value & 0xFF)
-        
+        self.register_write(TCA9354_REG_OUTPUT, value & 0xFF)
+
+    def write_pin(self, pin, value):
+        """
+        Read the current state of the output, then update it to set one output pin high/low.
+
+        Args:
+            pin: The number of the pin to update. Values outside the range [0..7] result in no operation taking place.
+            value: Whether the pin should be set high (True) or low (False).
+        """
+        if pin < 0 or pin > 7:
+            return
+        if value:
+            self.register_update(TCA9354_REG_OUTPUT, (1 << pin), 0)
+        else:
+            self.register_update(TCA9354_REG_OUTPUT, 0, (1 << pin))
